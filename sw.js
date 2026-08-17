36899ba497661626d1cb53d7f580d05709dd29b7
shafin830 <shafin830@gmail.com>
2026-08-17 01:12:40 -0500

    Bump cache after loop fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "santa-fe-trip-github-v4-before-roll-logic";
+const CACHE_NAME = "santa-fe-trip-github-v5-loop-fix";
 const ASSETS = ["./", "./index.html", "./styles.css", "./map-progress-fix.css", "./app.js", "./map-progress-fix.js", "./before-roll-fix.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
